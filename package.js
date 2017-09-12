0e708bf0612f9e30f01a7a751ff4e568bdae400e
David Baxter <davebaxter@gmail.com>
2017-09-12 15:10:39 +1000

    Change name

--- package.js
+++ package.js
@@ -1,18 +1,18 @@
 Package.describe({
-  name: 'rafaelhdr:google-charts',
+  name: 'supaseca:google-charts',
   summary: 'Easy Google Charts for Meteor',
   version: '0.0.8',
-  git: 'https://github.com/rafaelhdr/meteor-google-charts.git'
+  git: 'https://github.com/projik/meteor-google-charts.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.4.2');
-  api.addFiles('rafaelhdr_google-charts.js');
+  api.addFiles('supaseca_google-charts.js');
   api.export(['drawChart'], 'client');
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('rafaelhdr:google-charts');
-  api.addFiles('rafaelhdr_google-charts-tests.js');
+  api.use('supaseca:google-charts');
+  api.addFiles('supaseca_google-charts-tests.js');
 });
